0ad82f72606e6db1ebf32a992ac67da8930474c7
Bhaskarjyoti Das <38781055+bhaskarjy0ti@users.noreply.github.com>
2018-07-14 01:08:59 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -25,13 +25,13 @@
 AUTOMOUNT=true
 
 # Set to true if you need to load system.prop
-PROPFILE=true
+PROPFILE=false
 
 # Set to true if you need post-fs-data script
 POSTFSDATA=true
 
 # Set to true if you need late_start service script
-LATESTARTSERVICE=true
+LATESTARTSERVICE=false
 
 ##########################################################################################
 # Installation Message
